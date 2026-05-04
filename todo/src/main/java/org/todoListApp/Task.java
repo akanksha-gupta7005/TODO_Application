f425b94ca71fbe1875c00021122d95c13d66ceb2
Akankshagupta09 <guptaakanksha08373@gmail.com>
2026-05-04 22:14:06 +0530

    Clean up whitespace in Task.java
    Removed unnecessary blank lines in Task.java

--- todo/src/main/java/org/todoListApp/Task.java
+++ todo/src/main/java/org/todoListApp/Task.java
@@ -1,18 +1,11 @@
 package org.todoListApp;
-
-
-
-
-
 import jakarta.persistence.*;
 import lombok.Data;
 import java.time.LocalDateTime;
-
 @Data
 @Entity
 @Table(name = "tasks")
 public class Task {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
